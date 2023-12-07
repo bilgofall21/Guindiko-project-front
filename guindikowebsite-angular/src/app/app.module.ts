5ffd53441c9d1fa3e6afa58a4b01fce4bdcd36cb
bilgofall21 <papaamadem@gmail.com>
2023-12-07 17:57:04 +0000

    ajout menu et footer

--- guindikowebsite-angular/src/app/app.module.ts
+++ guindikowebsite-angular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header-footer/header/header.component';
+import { FooterComponent } from './header-footer/footer/footer.component';
+import { BgColorBlueguindikoDirective } from './bg-color-blueguindiko.directive';
+import { BgColorOrangeguindikoDirective } from './bg-color-orangeguindiko.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BgColorBlueguindikoDirective,
+    BgColorOrangeguindikoDirective
   ],
   imports: [
     BrowserModule,
