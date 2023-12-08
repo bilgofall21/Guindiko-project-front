3dbe597e8b157ffce9a1129c2faf0a1a9bd6c801
Vevebel <diatta.veroniquemarie@gmail.com>
2023-12-08 10:42:15 +0000

    ceci est un  commit

--- guindikowebsite-angular/src/app/app.module.ts
+++ guindikowebsite-angular/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { NosServicesComponent } from './serviceGuindoko/nos-services/nos-services.component';
+import { DetailInformatiqueComponent } from './serviceGuindoko/detail-informatique/detail-informatique.component';
+import { DetailGestionManagementComponent } from './serviceGuindoko/detail-gestion-management/detail-gestion-management.component';
+import { DetailTechniqueComponent } from './serviceGuindoko/detail-technique/detail-technique.component';
+import { DetailHotellerieComponent } from './serviceGuindoko/detail-hotellerie/detail-hotellerie.component';
+import { DetailScienceComponent } from './serviceGuindoko/detail-science/detail-science.component';
+import { DetailArtComponent } from './serviceGuindoko/detail-art/detail-art.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    NosServicesComponent,
+    DetailInformatiqueComponent,
+    DetailGestionManagementComponent,
+    DetailTechniqueComponent,
+    DetailHotellerieComponent,
+    DetailScienceComponent,
+    DetailArtComponent
   ],
   imports: [
     BrowserModule,
