07a5e6972a88f5ee84a9559a0fc7335a2e20fbad
bilgofall21 <papaamadem@gmail.com>
2023-12-09 17:08:12 +0000

    page articles et detail articles

--- guindikowebsite-angular/src/app/app-routing.module.ts
+++ guindikowebsite-angular/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ListesMentorComponent } from './mentor/listes-mentor/listes-mentor.component';
+import { ArticleHomeComponent } from './page-articles/article-home/article-home.component';
+import { DetailsArticleComponent } from './page-articles/details-article/details-article.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component : AccueilComponent,},
+  {path : 'listes-mentor', component :ListesMentorComponent,},
+  {path : 'article-home' , component : ArticleHomeComponent,},
+  {path : 'details-article', component : DetailsArticleComponent,},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- guindikowebsite-angular/src/app/mentor/listes-mentor/listes-mentor.component.ts
+++ guindikowebsite-angular/src/app/mentor/listes-mentor/listes-mentor.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-listes-mentor',
   templateUrl: './listes-mentor.component.html',
