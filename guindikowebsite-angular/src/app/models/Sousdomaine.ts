3a0d9bcffa3ab9d4f640f1e860d21c75323db74a
mzelley <colymarielaure@gmail.com>
2023-12-11 00:37:50 +0000

    fonctionnalité ajouter

---
+++ guindikowebsite-angular/src/app/models/Sousdomaine.ts
@@ -0,0 +1,10 @@
+export class Sousdomaine {
+    _id!: string;
+    nom!: string;
+    image!: string;
+    description!: string;
+    createdAt!: string;
+    updatedAt!: string;
+    createdBy!: string;
+    updatedBy!: string;
+  }
