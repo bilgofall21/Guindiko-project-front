75abfacf558a756a2bff92b9de1e7ad70d7e3c87
Vevebel <diatta.veroniquemarie@gmail.com>
2023-12-11 14:19:04 +0000

    ceci est mon premier commit de ce jour

--- guindikowebsite-angular/src/app/serviceGuindoko/nos-services/nos-services.component.ts
+++ guindikowebsite-angular/src/app/serviceGuindoko/nos-services/nos-services.component.ts
@@ -1,31 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nos-services',
   templateUrl: './nos-services.component.html',
   styleUrls: ['./nos-services.component.css']
 })
-export class NosServicesComponent {
-  // searchTerm: string = '';
+export class NosServicesComponent implements OnInit {
 
-  // filtrerParNom = (item: any) => {
-  //   // Ajoutez ici la logique de filtrage par nom
-  //   // Vous pouvez utiliser this.searchTerm pour accéder à la valeur de recherche
-  //   return item.nom.toLowerCase().includes(this.searchTerm.toLowerCase());
-  // };
-  filterValue = '';
-  filteredElement: any;
+  domaines : any []=[
+    {
+      id :1,
+      titreDomaine: "Informatique",
+      description : " Le mentorat en informatique, c’est bien plus qu’une profession, c’est une passerelle vers l’excellence numérique. En tant que mentor en informatique, vous avez le pouvoir de guider les esprits avides de connaissances à travers le paysage complexe de la technologie, en partageant votre expertise ....",
+      imageDomaine : "../../../assets/fullProject/imgServive/informatique.png"
+    },
+    {
+      id :2,
+      titreDomaine: "Sciences",
+      description : " Le mentorat en sciences est une aventure passionnante vers la découverte,  l’apprentis-  sage et l’innovation. En tant que mentor en sciences, vous avez le privilège de g     uider des esprits curieux avides de comprendre les mystères de l’univers, en partageant votre expertise pour les inspirer à explorer,.... ",
+      imageDomaine : "../../../assets/fullProject/imgServive/science.png"
+    },
+    {
+      id :3,
+      titreDomaine: "Gestion et Management",
 
-  // ... Autres propriétés et méthodes
+      description : "Le mentorat en gestion est une profession dédiée à façonner les futurs leaders et à renforcer les compétences en gestion. En tant que mentor en gestion, vous partagez votre expérience et vos connaissances pour guider les aspirants gestionnaires dans la compréhension des défis complexe .......",
+      imageDomaine: "../../../assets/fullProject/imgServive/gestman.png"
+    },
+    {
+      id :4,
+      titreDomaine: "Art",
 
-  // Méthode de recherche automatique
-//   onSearch() {
-//     // La recherche se fait selon le nom ou le prénom
-//     this.filteredElement = this.tabDomaine.filter(
-//       (elt: any) =>
-//         (elt?.domaine.toLowerCase().includes(this.filterValue.toLowerCase())) ||
-//         elt?.prenomContact.toLowerCase().includes(this.filterValue.toLowerCase())
-//     );
+      description : " Le mentorat en art est une forme d’accompagnement artistique qui transcende les techniques et les médiums. En tant que mentor en art, vous avez le pouvoir de guider des artistes en herbe, des créateurs et des passionnés d’art dans leur voyage de découverte artistique, ....",
+      imageDomaine: "../../../assets/fullProject/imgServive/art.png"
+    },
+    {
+      id :5,
+      titreDomaine: "Technique",
+
+      description : "Le mentorat en technique est une opportunité de partager une expertise technique précieuse avec ceux qui cherchent à maîtriser les aspects pratiques et les compétences essentielles de divers domaines techniques. En tant que mentor en technique, vous guidez les apprenants, les étudiants ....",
+      imageDomaine: "../../../assets/fullProject/imgServive/technique.png"
+    },
+    {
+      id :6,
+      titreDomaine: "Hôtellerie",
+      description : " Le mentorat en  hôtellerie vous ouvre les portes d'une aventure exceptionnelle, taillée sur mesure pour façonner votre chemin vers l'excellence professionnelle. C'est une immersion personnalisée dans le monde dynamique de l'accueil et de la gestion hôtelière,....",
+      imageDomaine: "../../../assets/fullProject/imgServive/hotelerie.png"
+    }
+
+  ];
+  filterValue: string = '';
+element: any;
+originalDomaines: any[] = [];
+// filterValue: string = '';
+
+ngOnInit(): void {
+
+  this.originalDomaines = [...this.domaines];
+}
+
+
+// onSearch() {
+//   // Implement your search logic here
+//   console.log('Search triggered with value:', this.filterValue);
+//   // Example: Filter the domaines array based on the title
+//   this.domaines = this.domaines.filter(element =>
+//     element.titreDomaine.toLowerCase().includes(this.filterValue.toLowerCase())
+//   );
 // }
+onSearch() {
+  // If the search input is empty, reset the domaines array
+  if (this.filterValue.trim() === '') {
+    this.domaines = [...this.originalDomaines];
+  } else {
+    // Filter the domaines array based on the title
+    this.domaines = this.originalDomaines.filter(element =>
+      element.titreDomaine.toLowerCase().includes(this.filterValue.toLowerCase())
+    );
+  }
+}
 
 }
